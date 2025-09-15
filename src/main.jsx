0f1f964376901140299f514b48da306057315e9f
sudhin4 <sudhinnaveen38@gmail.com>
2025-09-15 20:33:06 +0530

    Some css chanegs

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './component/Header.jsx'
-import Mainsection from './component/Mainsection.jsx'
-import TestimonialComponent from './component/Testimonialcomponent.jsx'
-import Lastproject from './component/LastProjects.jsx'
-import Aboutussection from './component/Aboutus.jsx'
-import Contactsection from './component/Contactsection.jsx'
-import Footer from './component/footer.jsx'
-import Rain from './component/Rain.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
